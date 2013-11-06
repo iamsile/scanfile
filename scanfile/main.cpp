4eee024c1301231bdf46a70f4efddf08d0d72b08
sile <iamsai007@sina.com>
2013-11-06 22:47:23 +0800

    scanfile number from directory

--- scanfile/main.cpp
+++ scanfile/main.cpp
@@ -7,12 +7,38 @@
 //
 
 #include <iostream>
-
-int main(int argc, const char * argv[])
+//扫描目录下的文件
+char get_file_name()
 {
+	system("find /tmp/ -type d >file_path_name.txt");
+	system("find /tmp/ -type d | wc -l >file_path_num.txt");
+	system("find /tmp/ -name '*' | wc -l >file_num.txt");
 
-	// insert code here...
-	std::cout << "Hello, World!\n";
-    return 0;
+	FILE *fp_cont, *fp_num;
+	file_msg file_mesgs;
+
+	fp_num = fopen("file_num.txt", "r");
+	fscanf(fp_num, "%d", &file_mesgs.nums);
+	fclose(fp_num);
+
+	fp_cont = fopen("file_path_num.txt", "r");
+	fscanf(fp_cont, "%", &file_mesgs.cont);
+	fclose(fp_cont);
+
+	printf("%d\n%d\n", file_mesgs.nums, file_mesgs.cont);
+
+	FILE *fp_path;
+	fp_path = fopen("file_path_name.txt", "r");
+	file_mesg files_mesgs[file_mesgs.cont];
+
+	for (int i = 1; i <= file_mesgs.cont; i++) {
+		fscanf(fp_path, "%s", files_mesgs[i].paths);
+		printf("%s\n",file_mesgs[i].paths);
+	}
 }
 
+int mian()
+{
+	get_file_name();
+	return 0;
+}
