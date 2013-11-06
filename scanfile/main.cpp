9df367621be7ef34a1f3ddd7107af6bb7a730941
sile <iamsai007@sina.com>
2013-11-06 22:38:22 +0800

    Initial Commit

---
+++ scanfile/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  scanfile
+//
+//  Created by taowei on 13-11-6.
+//  Copyright (c) 2013年 taowei. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+	// insert code here...
+	std::cout << "Hello, World!\n";
+    return 0;
+}
+
